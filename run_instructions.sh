9ea6e7f32ddaaf2e981c903a1769681f3bb9fd85
DimaAmega <dima.amega1@gmail.com>
2021-09-01 07:18:44 +0300

    FIXED SH FILES

--- run_instructions.sh
+++ run_instructions.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
-
 set -e
 
-if [ -f $1 ]; then
-    cat $1 | ghci $2 -fno-show-loaded-modules | sed \
-    -e '/Ok, [A-Za-z0-9]* modules loaded./,$!d' \
-    -e '/Ok, [A-Za-z0-9]* modules loaded./d' \
+
+CURDIR=`/bin/pwd`
+BASEDIR=$(dirname $0)
+
+echo "CURDIR is $CURDIR"
+echo "BASEDIR is $BASEDIR"
+
+cd $1
+
+if [ -f $2 ]; then
+    cat $2 | ghci $3 -fno-show-loaded-modules | sed \
+    -e '/Ok, [A-Za-z0-9]* module\(s*\) loaded./,$!d' \
+    -e '/Ok, [A-Za-z0-9]* module\(s*\) loaded./d' \
     -e 's/*[A-Z][a-z0-9]*> *//' \
     -e '$ d'
     exit 0
